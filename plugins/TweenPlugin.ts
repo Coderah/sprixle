51a892319c5982d911dd2e73ab26ce12fbabd542
Coderah <alex@howardsupply.com>
2024-06-29 14:45:31 -0700

    rename TweenPlugin and add support for number[]

--- plugins/TweenPlugin.ts
+++ plugins/TweenPlugin.ts
@@ -1,19 +1,19 @@
 import { lerp } from 'three/src/math/MathUtils';
-import { Manager, defaultComponentTypes } from '../ecs/manager';
+import { Keys, Manager, defaultComponentTypes } from '../ecs/manager';
 import { now } from '../util/now';
 
 export type TweenComponents<ComponentTypes extends defaultComponentTypes> = {
-    tweenTarget: Manager<ComponentTypes>['Entity'];
-    tweeningComponent: keyof ComponentTypes;
-    tweenTo: number;
+    tweenTargetId: string;
+    tweeningComponent: Keys<ComponentTypes>;
+    tweenTo: number | number[];
     tweenLength: number;
-    tweenFrom: number;
+    tweenFrom: number | number[];
     tweenStart: number;
 };
 
 export const TWEEN_COMPONENT_DEFAULTS: TweenComponents<defaultComponentTypes> =
     {
-        tweenTarget: undefined,
+        tweenTargetId: '',
         tweeningComponent: 'updatedAt',
         tweenTo: 0,
         tweenLength: 0,
@@ -21,12 +21,13 @@ export const TWEEN_COMPONENT_DEFAULTS: TweenComponents<defaultComponentTypes> =
         tweenStart: 0,
     };
 
+// TODO figure out typing to force only number | number[] components within tween functions
 export function applyTweenPlugin<
     ComponentTypes extends defaultComponentTypes &
         TweenComponents<ComponentTypes>
 >(manager: Manager<ComponentTypes>) {
     const tweenerQuery = manager.createQuery({
-        includes: ['tweenTarget'],
+        includes: ['tweenTargetId'],
     });
 
     return {
@@ -34,13 +35,15 @@ export function applyTweenPlugin<
         tweenSystem: manager.createSystem(tweenerQuery, {
             all(entity, delta) {
                 const {
-                    tweenTarget,
+                    tweenTargetId,
                     tweeningComponent,
                     tweenTo,
                     tweenLength,
                     tweenFrom,
                     tweenStart,
                 } = entity.components;
+
+                const tweenTarget = manager.getEntity(tweenTargetId);
 
                 if (
                     !tweenTarget ||
@@ -55,39 +58,61 @@ export function applyTweenPlugin<
                 const time = now();
 
                 const tweenDelta = (time - tweenStart) / tweenLength;
+
                 if (tweenDelta > 1) {
                     tweenTarget.components[tweeningComponent] = tweenTo;
                     manager.deregisterEntity(entity);
                 } else {
-                    tweenTarget.components[tweeningComponent] = lerp(
-                        tweenFrom,
-                        tweenTo,
-                        tweenDelta
-                    );
+                    const component = tweenTarget.components[tweeningComponent];
+                    if (component.length) {
+                        (component as number[]).forEach((v, i) => {
+                            component[i] = lerp(
+                                tweenFrom[i],
+                                tweenTo[i],
+                                tweenDelta
+                            );
+                            console.log(
+                                '[TweenSystem]',
+                                component,
+                                i,
+                                tweenFrom[i],
+                                tweenTo[i]
+                            );
+                        });
+
+                        tweenTarget.flagUpdate(tweeningComponent);
+                    } else {
+                        tweenTarget.components[tweeningComponent] = lerp(
+                            tweenFrom as number,
+                            tweenTo as number,
+                            tweenDelta
+                        );
+                    }
                 }
             },
             // cleanup(entity) {},
         }),
-        tween(
+        tween<C extends Keys<ComponentTypes>, V extends number | number[]>(
             entity: typeof manager.Entity,
-            tweeningComponent: keyof ComponentTypes,
-            tweenTo: number,
+            tweeningComponent: C,
+            tweenTo: V,
             tweenLength: number,
-            tweenFrom = entity.components[tweeningComponent] as any as number,
+            tweenFrom = entity.components[tweeningComponent] as any as V,
             tweenStart = now()
         ) {
-            const tweenerId = entity.id + tweeningComponent + 'tween';
+            const tweenerId =
+                entity.id + tweeningComponent.toString() + 'tween';
 
-            const tweenEntity = manager.entityExists(tweenerId)
-                ? manager.getEntity(tweenerId)
-                : manager.createEntity(tweenerId);
-            tweenEntity.components.tweenTarget = entity;
+            let tweenEntity = manager.getEntity(tweenerId);
+
+            if (!tweenEntity) tweenEntity = manager.createEntity(tweenerId);
+
+            tweenEntity.components.tweenTargetId = entity.id;
             tweenEntity.components.tweeningComponent = tweeningComponent;
             tweenEntity.components.tweenTo = tweenTo;
             tweenEntity.components.tweenLength = tweenLength;
             tweenEntity.components.tweenFrom = tweenFrom;
             tweenEntity.components.tweenStart = tweenStart;
-            manager.registerEntity(tweenEntity);
 
             return tweenEntity;
         },

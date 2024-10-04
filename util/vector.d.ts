f3285a1302ab7927983f965af202adcaac6545c3
Alex Howard <alex@howardsupply.com>
2024-10-04 15:30:57 -0700

    Vector2.x[Y][Z] and Vector3.rgb utilities

--- util/vector.d.ts
+++ util/vector.d.ts
@@ -4,5 +4,13 @@ declare module 'three' {
     export interface Vector3 {
         get xy(): Vector2;
         get xz(): Vector2;
+        get r(): number;
+        get g(): number;
+        get b(): number;
+    }
+
+    export interface Vector2 {
+        get xYz(): Vector3;
+        get xyZ(): Vector3;
     }
 }
--- util/vector.ts
+++ util/vector.ts
@@ -15,3 +15,34 @@ Object.defineProperty(Vector3.prototype, 'xy', {
         return xyVector.set(this.x, this.y);
     },
 });
+
+const xyzVector = new Vector3();
+Object.defineProperty(Vector2.prototype, 'xyZ', {
+    get() {
+        return xyzVector.set(this.x, 0, this.y);
+    },
+});
+
+Object.defineProperty(Vector2.prototype, 'xYz', {
+    get() {
+        return xyzVector.set(this.x, this.y, 0);
+    },
+});
+
+Object.defineProperty(Vector3.prototype, 'r', {
+    get() {
+        return this.x;
+    },
+});
+
+Object.defineProperty(Vector3.prototype, 'g', {
+    get() {
+        return this.y;
+    },
+});
+
+Object.defineProperty(Vector3.prototype, 'b', {
+    get() {
+        return this.z;
+    },
+});

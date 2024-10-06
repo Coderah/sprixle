ee546b043f9e86277a5780ec19cc60b941e8853a
Alex Howard <alex@howardsupply.com>
2024-10-05 17:47:01 -0700

    [blender] getFeaturesFromName checks material too

--- util/blender.ts
+++ util/blender.ts
@@ -1,17 +1,20 @@
-import { Object3D } from 'three';
+import { Material, Object3D } from 'three';
 
 export function getFeaturesFromName(o: {
     name: string;
     userData?: Object3D['userData'];
+    material?: Material;
 }) {
     const features: {
         [key: string]: boolean | string;
     } = {};
 
     Array.from(
-        ((o.userData?.name as string) || o.name).matchAll(
-            /\+([\w]+)(?:\(([\w]+)\))?/gi
-        )
+        (
+            ((o.userData?.name as string) || o.name) +
+            ' ' +
+            ((o.material?.userData.name as string) || o.material?.name || '')
+        ).matchAll(/\+([\w]+)(?:\(([\w-]+)\))?/gi)
     ).forEach((m) => {
         if (m[1]) {
             features[m[1]] = m[2] || true;

c79ac79bf4733651a559435a71b0f928da9426c3
Alex Howard <alex@howardsupply.com>
2025-03-20 16:14:57 -0700

    [nodeTrees] add clamp to reservedWords

--- plugins/nodeTrees/util.ts
+++ plugins/nodeTrees/util.ts
@@ -7,7 +7,7 @@ import {
     TypeMethod,
 } from '@deepkit/type';
 
-const reservedWords = new Set(['mix', 'attribute']);
+const reservedWords = new Set(['mix', 'attribute', 'clamp']);
 export function getReference(n: Node): string;
 export function getReference(n: string): string;
 export function getReference(n: string | Node): string {

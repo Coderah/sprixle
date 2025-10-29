7135902aa8c63be6c06731ce195bb05738e94c30
Alex Howard <alex@howardsupply.com>
2025-10-28 20:49:49 -0700

    improve patchHandlers.test.ts

--- test/patchHandlers.test.ts
+++ test/patchHandlers.test.ts
@@ -1,20 +1,12 @@
+import { getBsonEncoder } from '@deepkit/bson';
+import { typeOf } from '@deepkit/type';
+import assert from 'assert';
 import {
     defaultComponentTypes,
-    Entity,
-    EntityId,
     Manager,
     SerializableEntity,
     SerializableState,
 } from '../ecs/manager';
-import {
-    deserializeBSON,
-    getBSONDeserializer,
-    getBsonEncoder,
-    getBSONSerializer,
-    serializeBSON,
-} from '@deepkit/bson';
-import { deserialize, serialize, typeOf } from '@deepkit/type';
-import assert from 'assert';
 
 type ComponentTypes = defaultComponentTypes & {
     serverOnly: Object;
@@ -25,11 +17,6 @@ type ComponentTypes = defaultComponentTypes & {
 type SerializableComponents = Omit<ComponentTypes, 'serverOnly'>;
 
 type TransmittableEntity = SerializableEntity<Partial<SerializableComponents>>;
-// type ComponentPatch = [
-//     EntityId,
-//     keyof ComponentTypes,
-//     ComponentTypes[keyof ComponentTypes]
-// ];
 
 const manager = new Manager<ComponentTypes>();
 
@@ -63,7 +50,7 @@ manager.patchHandlers = {
         const newEntity = entityB.decode(serialized);
         console.log(
             '[patchHandlers register]',
-            serialized.length,
+            serialized.byteLength,
             // deserialize<typeof manager.Entity>(serialized),
             newEntity,
             'took',
@@ -91,7 +78,12 @@ manager.patchHandlers = {
         // );
 
         console.log('deserialized', entityB.decode(serialized));
-        console.log('serialize loop', components, performance.now() - time);
+        console.log(
+            'serialize loop took',
+            performance.now() - time,
+            'byte size',
+            serialized.byteLength
+        );
     },
     deregister(entity) {
         console.log('[patchHandlers deregister]', entity);
@@ -111,6 +103,7 @@ entity.components.genericID = '8259-2348235-2348234-23424';
 entity.components.numericComponent += 3;
 manager.subTick();
 
+entity.components.serverOnly = window;
 entity.components.genericID = '234-2348235-2348234-23424';
 entity.components.numericComponent += 3;
 manager.subTick();
@@ -120,7 +113,6 @@ entity.components.genericID = '8259-55-2348234-23424';
 entity.components.numericComponent += 3;
 manager.subTick();
 
-entity.components.genericID = '8259-2348235-88-23424';
 entity.components.numericComponent += 3;
 
 // test stagedUpdates serialization
@@ -131,7 +123,7 @@ manager.tick();
 
 const time = performance.now();
 const serializedState = stateB.encode(manager.state);
-console.log('serialized manager state', serializedState);
+console.log('serialized manager state byte size', serializedState.byteLength);
 console.log('took', performance.now() - time);
 
 console.log(stateB.decode(serializedState));

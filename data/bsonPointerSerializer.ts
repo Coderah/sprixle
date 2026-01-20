9623ad2614829c6aba9cb6121f8406e3cfabd7a4
Alex Howard <alex@howardsupply.com>
2026-01-20 13:50:16 -0800

    [pointers] fix undefined pointer serialization

--- data/bsonPointerSerializer.ts
+++ data/bsonPointerSerializer.ts
@@ -54,23 +54,22 @@ bsonBinarySerializer.bsonSerializeRegistry.prepend(
         });
 
         const start = state.compilerContext.reserveName('start');
-        serializePropertyNameAware(
-            type,
-            state,
-            BSONType.string,
-            null,
-            `
-            var ${start} = state.writer.offset;
-            const key = registry.reverse.get(${state.accessor});
-            if (key === undefined) {
-                throw new Error('[Pointer Serialization] Object not found in registry for "${dataSourceName}"');
+        state.template = `
+            if (${state.accessor} !== undefined) {
+                var ${start} = state.writer.offset;
+                const key = registry.reverse.get(${state.accessor});
+                if (key === undefined) {
+                    throw new Error('[Pointer Serialization] Object not found in registry for "${dataSourceName}"');
+                }
+                state.writer.writeType(${BSONType.string});
+                state.writer.offset += 4; //size placeholder
+                state.writer.writeString(key);
+                state.writer.writeByte(0); //null
+                state.writer.writeDelayedSize(state.writer.offset - ${start} - 4, ${start});
+            } else {
+                state.writer.writeType(${BSONType.undefined});   
             }
-            state.writer.offset += 4; //size placeholder
-            state.writer.writeString(key);
-            state.writer.writeByte(0); //null
-            state.writer.writeDelayedSize(state.writer.offset - ${start} - 4, ${start});
-            `
-        );
+            `;
 
         state.stop();
     }
@@ -96,18 +95,14 @@ bsonBinarySerializer.sizerRegistry.prepend(
             getValueSize,
         });
 
-        sizerPropertyNameAware(
-            type,
-            state,
-            null,
-            `
-            const key = registry.reverse.get(${state.accessor});
-            if (key === undefined) {
-                throw new Error('[Pointer Sizing] Object not found in registry for "${dataSourceName}"');
-            }
-            state.size += getValueSize(key);
-            `
-        );
+        state.template = `
+            if (${state.accessor} !== undefined) {
+                const key = registry.reverse.get(${state.accessor});
+                if (key === undefined) {
+                    throw new Error('[Pointer Sizing] Object not found in registry for "${dataSourceName}"');
+                }
+                state.size += getValueSize(key);
+            }`;
 
         state.stop();
     }
@@ -141,6 +136,8 @@ bsonBinarySerializer.bsonDeserializeRegistry.prepend(
                     throw new Error('[Pointer Deserialization] Key "' + key + '" not found in registry for "${dataSourceName}"');
                 }
                 ${state.setter} = value;
+            } else if (state.elementType === ${BSONType.undefined}) {
+                ${state.setter} = undefined;
             } else {
                 throw new Error('[Pointer Deserialization] Expected string type for "${dataSourceName}", got ' + state.elementType);
             }
